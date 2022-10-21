b91b13219660332228f7799272bc7831ec0518e4
karthikmanjunathk <karthikmanjunath27@gmail.com>
2022-10-21 22:53:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//import express from 'express'; //replace it with const express = require('express');
+//node -v = v16.18.0  npm - v = 8.19.2
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000;            //In production heroku will provide the port number, for local we can specify 5000
+app.listen(PORT);
