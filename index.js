6cf3f1bcd4da3b89f9f836d15c25743aeaedb825
karthikmanjunathk <karthikmanjunath27@gmail.com>
2022-10-25 22:16:57 +0530

    "From VS code"

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 //node -v = v16.18.0  npm - v = 8.19.2
 
 const express = require('express');
+require('./services/passport');     // We are not exporting from the passport.js so no need to create const
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({bye: 'brother'});
-});
+require('./routes/authRoutes')(app);     //reuire will return a function, we then immediately call that function with app object
 
 const PORT = process.env.PORT || 5000;            //In production heroku will provide the port number, for local we can specify 5000
 app.listen(PORT);
